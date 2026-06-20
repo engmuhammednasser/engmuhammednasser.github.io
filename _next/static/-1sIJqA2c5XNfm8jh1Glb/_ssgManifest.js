cc034e51e270343b9167aacfdbcea94019247a3a
Muhammed Nasser <engmuhammednasser@example.com>
2026-06-20 20:15:49 +0300

    Deploy: reorder botella to be before techmart

---
+++ _next/static/-1sIJqA2c5XNfm8jh1Glb/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Far\u002Fbackend\u002F[slug]","\u002Far\u002Flab\u002Fplugins\u002F[slug]","\u002Far\u002Fwork\u002F[slug]","\u002Fbackend\u002F[slug]","\u002Flab\u002Fplugins\u002F[slug]","\u002Fwork\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
