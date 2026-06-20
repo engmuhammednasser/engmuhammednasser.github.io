d8d35370596702976435079c7833598f867c7828
Muhammed Nasser <engmuhammednasser@example.com>
2026-06-20 04:25:37 +0300

    feat: migrate site to root directory without demo prefix

--- demo/_next/static/-I4h_AjidTqfLzNEQl3tk/_clientMiddlewareManifest.js
+++
@@ -1,6 +0,0 @@
-self.__MIDDLEWARE_MATCHERS = [
-  {
-    "regexp": "^\\/demo(?:\\/(_next\\/data\\/[^/]{1,}))?(?:\\/((?!_next|api|projects|logos|plugins|backend|favicon.ico|favicon.png|profile.png|icons.svg).*))(\\.json|\\.rsc|\\.segments\\/.+\\.segment\\.rsc)?[\\/#\\?]?$",
-    "originalSource": "/((?!_next|api|projects|logos|plugins|backend|favicon.ico|favicon.png|profile.png|icons.svg).*)"
-  }
-];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
